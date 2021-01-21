0d9cd6f56de6f83f494697b7f5d81f9866edad81
Therese Lybo <therese.lybo@gmail.com>
2021-01-21 23:07:12 +0100

    fade in on scroll, projects page

--- pages/projects/Project.js
+++ pages/projects/Project.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Link from "next/link";
 import { Card, Col, Row, Button } from "react-bootstrap";
 import LinkIcons from "./LinkIcons";
+import Fade from "react-reveal/Fade";
 
 function Project(props) {
   const [isVisible, setIsVisible] = useState(false);
@@ -9,72 +10,61 @@ function Project(props) {
   return (
     <div className="mb-5">
       {/* MOBILE DISPLAY */}
-      <Card className="card-vertical mb-5 d-block d-md-none">
-        <Card.Img variant="top" src={props.image} alt="Project image" />
-        <Card.Body>
-          <Card.Title>{props.title}</Card.Title>
-          <Card.Subtitle>{props.techs}</Card.Subtitle>
-          <Card.Text className="mt-3">{props.intro}</Card.Text>
-          <LinkIcons
-            case_study={props.case_study}
-            github_repo={props.github_repo}
-            live_project={props.live_project}
-          />
-        </Card.Body>
-      </Card>
+      <Fade bottom>
+        <Card className="card-vertical mb-5 d-block d-md-none">
+          <Card.Img variant="top" src={props.image} alt="Project image" />
+          <Card.Body>
+            <Card.Title>{props.title}</Card.Title>
+            <Card.Subtitle>{props.techs}</Card.Subtitle>
+            <Card.Text className="mt-3">{props.intro}</Card.Text>
+            <LinkIcons
+              case_study={props.case_study}
+              github_repo={props.github_repo}
+              live_project={props.live_project}
+            />
+          </Card.Body>
+        </Card>
+      </Fade>
 
       {/* DESKTOP DISPLAY */}
-      <Card
-        className="card-horizontal d-none d-md-block"
-        onMouseEnter={() => setIsVisible(true)}
-        onMouseLeave={() => setIsVisible(false)}
-      >
-        <Row>
-          <Col className="pr-0">
-            <Card.Body className="p-4 d-flex flex-column">
-              <Card.Title>{props.title}</Card.Title>
-              <Card.Subtitle>{props.techs}</Card.Subtitle>
-              <Card.Text className="mt-3">{props.intro}</Card.Text>
-            </Card.Body>
-          </Col>
-          <Col className="pl-0 img-container position-relative">
-            <div
-              className="project-image h-100"
-              style={{ backgroundImage: `url(${props.image})` }}
-            >
-              <div className="hover-indicator"></div>
-              {isVisible && (
-                <div
-                  className={`project-hover my-auto d-flex align-items-center ${
-                    isVisible ? "transition-opacity" : ""
-                  }`}
-                >
-                  <LinkIcons
-                    case_study={props.case_study}
-                    github_repo={props.github_repo}
-                    live_project={props.live_project}
-                  />
-                </div>
-              )}
-            </div>
-            {/* <Card.Img src={props.image} />
-            <div className="hover-indicator"></div>
-            {isVisible && (
+      <Fade bottom>
+        <Card
+          className="card-horizontal d-none d-md-block"
+          onMouseEnter={() => setIsVisible(true)}
+          onMouseLeave={() => setIsVisible(false)}
+        >
+          <Row>
+            <Col className="pr-0">
+              <Card.Body className="p-4 d-flex flex-column">
+                <Card.Title>{props.title}</Card.Title>
+                <Card.Subtitle>{props.techs}</Card.Subtitle>
+                <Card.Text className="mt-3">{props.intro}</Card.Text>
+              </Card.Body>
+            </Col>
+            <Col className="pl-0 img-container position-relative">
               <div
-                className={`project-hover my-auto ${
-                  isVisible ? "transition-opacity" : ""
-                }`}
+                className="project-image h-100"
+                style={{ backgroundImage: `url(${props.image})` }}
               >
-                <LinkIcons
-                  case_study={props.case_study}
-                  github_repo={props.github_repo}
-                  live_project={props.live_project}
-                />
+                <div className="hover-indicator"></div>
+                {isVisible && (
+                  <div
+                    className={`project-hover my-auto d-flex align-items-center ${
+                      isVisible ? "transition-opacity" : ""
+                    }`}
+                  >
+                    <LinkIcons
+                      case_study={props.case_study}
+                      github_repo={props.github_repo}
+                      live_project={props.live_project}
+                    />
+                  </div>
+                )}
               </div>
-            )} */}
-          </Col>
-        </Row>
-      </Card>
+            </Col>
+          </Row>
+        </Card>
+      </Fade>
     </div>
   );
 }
