bbfa675c99747ea2354dba025a6a336153756fff
Therese Lybo <therese.lybo@gmail.com>
2021-01-22 13:18:25 +0100

    added fade in to nav

--- components/Header.js
+++ components/Header.js
@@ -1,26 +1,29 @@
 import React from "react";
 import { Container, Navbar, Nav } from "react-bootstrap";
+import Fade from "react-reveal/Fade";
 
 function Header() {
   return (
     <Navbar expand="md" className="my-3">
-      <Container>
+      <Fade bottom>
+        <Container>
           <Navbar.Brand href="/">
             <img src="/images/logo.png" alt="Logo" width="120px" />
           </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse className="justify-content-end">
-          <Nav.Link className="px-0 px-md-3 mx-md-3" href="/">
-            Home
-          </Nav.Link>
-          <Nav.Link className="px-0 px-md-3 mx-md-3" href="/projects">
-            Projects
-          </Nav.Link>
-          <Nav.Link className="px-0 px-md-3 mx-md-3" href="/contact">
-            Contact
-          </Nav.Link>
-        </Navbar.Collapse>
-      </Container>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse className="justify-content-end">
+            <Nav.Link className="px-0 px-md-3 mx-md-3" href="/">
+              Home
+            </Nav.Link>
+            <Nav.Link className="px-0 px-md-3 mx-md-3" href="/projects">
+              Projects
+            </Nav.Link>
+            <Nav.Link className="px-0 px-md-3 mx-md-3" href="/contact">
+              Contact
+            </Nav.Link>
+          </Navbar.Collapse>
+        </Container>
+      </Fade>
     </Navbar>
   );
 }
