cbd9e5432d28103487027cf7c43c7b4d329cec6b
Therese Lybo <therese.lybo@gmail.com>
2021-01-27 12:30:05 +0100

    new sub domains

--- pages/projects/index.js
+++ pages/projects/index.js
@@ -21,7 +21,7 @@ export default function Projects() {
           intro="Oppgaven er å bygge en mikroside for SpaceX eller NASA for å øke bevissthet rundt aktivitet i verdensrommet. Mikrosiden skal bygges opp av fire mobilresponsive sider, og skal blant annet inneholde en tidslinje over rakettutskytninger, et JavaScript-validert kontaktskjema, og linker til ytterligere informasjon."
           case_study="/post/3-caste-study-spacex"
           github_repo="https://github.com/thereselybo/spacex"
-          live_project="https://lybo.dev/projex1"
+          live_project="https://spacex.lybo.dev/"
           image="/images/spacex-screenshot.jpg"
         />
 
@@ -31,11 +31,11 @@ export default function Projects() {
           intro="Oppgaven er å redesigne og rekonstruere en nettside, basert på IxD-prinsipper kombinert med kunnskap fra tidligere fag. Målet er å trene på ferdigheter oppnådd fra Interaksjonsdesign-faget, samt ferdigheter i HTML, CSS og JavaScript."
           case_study="#"
           github_repo="https://github.com/thereselybo/shadowball"
-          live_project="http://lybo.dev/ixd/ca-v2"
+          live_project="https://shadowball.lybo.dev/"
           image="/images/shadowball-desktop.jpg"
         />
 
-        <Project
+        {/* <Project
           title="Flower Power"
           techs={["HTML", "CSS", "Javascript", "CMS"]}
           intro="Oppgaven er å bygge én enkeltstående side som henter produkter fra et Wordpress REST API. Produktoversikten skal være en enkeltstående side, bygd opp av HTML, CSS og JavaScript, og skal inkludere produktnavn, bilder og priser. Hvert produkt skal ha en call-to-action-knapp for å se mer."
@@ -43,7 +43,7 @@ export default function Projects() {
           github_repo="https://github.com/thereselybo/flower-power"
           live_project="https://lybo.dev/cms/ca/"
           image="/images/flowerpower-screenshot.jpg"
-        />
+        /> */}
 
         <Project
           title="Community Science Museum"
@@ -51,7 +51,7 @@ export default function Projects() {
           intro="Oppgaven er å presentere et mobilresponsivt nettsted inneholdende minst 4-6 sider for et lokalt interaktivt vitenskapsmuseum. Nettstedet skal være informativt og tiltalende for unge elever og deres foreldre."
           case_study="/post/2-caste-study-science-museum"
           github_repo="https://github.com/thereselybo/science-museum"
-          live_project="https://lybo.dev/semproj1-v2/"
+          live_project="https://sciencemuseum.lybo.dev/"
           image="/images/science-museum-desktop.jpg"
         />
 
@@ -61,7 +61,7 @@ export default function Projects() {
           intro="Oppgaven er å bygge en mobilresponsiv nettside for et selskap som tilbyr miljøvennlige, guidede sykkelturer. Den bør bestå av minst 4 sider: hjemmeside, om oss, kontakt, og en oversikt over rutene."
           case_study="#"
           github_repo="https://github.com/thereselybo/re-cycle"
-          live_project="https://lybo.dev/htmlcss/ca-v2/"
+          live_project="https://recycle.lybo.dev/"
           image="/images/recycle-collection.jpg"
         />
       </Container>
