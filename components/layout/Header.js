af3dbb86a21274502dc32e228ea6e1460e3f4be4
Therese <therese.lybo@gmail.com>
2021-06-06 01:08:02 +0200

    Style mobile nav properly

--- components/layout/Header.js
+++ components/layout/Header.js
@@ -1,49 +1,36 @@
-import React from "react";
-import Link from "next/link";
 import { useRouter } from "next/router";
 
-import { Container, Navbar } from "react-bootstrap";
+import Link from "next/link";
+import { Container, Navbar, Nav } from "react-bootstrap";
 import Fade from "react-reveal/Fade";
 
 const Header = () => {
   const router = useRouter();
+  const path = router.pathname;
 
   return (
     <Fade bottom>
-      <Navbar expand="md" className="my-3">
+      <Navbar variant="custom" expand="md" className="my-3">
         <Container>
           <Link href="/" className="navbar-brand">
             <img src="/images/logo.png" alt="Logo" width="120px" />
           </Link>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse className="justify-content-end">
-            <Link href="/">
-              <a
-                className={`px-0 px-md-3 nav-link ${
-                  router.pathname == "/" || "" ? "active" : ""
-                }`}
-              >
-                Home
-              </a>
-            </Link>
-            <Link href="/projects">
-              <a
-                className={`px-0 px-md-3 nav-link ${
-                  router.pathname == "/projects" ? "active" : ""
-                }`}
-              >
-                Projects
-              </a>
-            </Link>
-            <Link href="/contact">
-              <a
-                className={`px-0 px-md-3 nav-link ${
-                  router.pathname == "/contact" ? "active" : ""
-                }`}
-              >
-                Contact
-              </a>
-            </Link>
+          <Navbar.Toggle aria-controls="responsive-navbar" />
+          <Navbar.Collapse
+            className="justify-content-end"
+            id="responsive-navbar"
+          >
+            <Nav activeKey={path}>
+              <Link href="/" passHref>
+                <Nav.Link className="px-0 px-md-3">Home</Nav.Link>
+              </Link>
+              <Link href="/projects" passHref>
+                <Nav.Link className="px-0 px-md-3">Projects</Nav.Link>
+              </Link>
+              <Link href="/contact" passHref>
+                <Nav.Link className="px-0 px-md-3">Contact</Nav.Link>
+              </Link>
+            </Nav>
           </Navbar.Collapse>
         </Container>
       </Navbar>
