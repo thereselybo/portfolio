79155dfa1ce7c801f3fc449114e60834541c2846
Therese <therese.lybo@gmail.com>
2021-06-05 19:51:34 +0200

    Translate 404 and add styles

--- pages/404.js
+++ pages/404.js
@@ -1,4 +1,4 @@
-import Layout from "../components/Layout";
+import Layout from "../components/layout/Layout";
 import Head from "next/head";
 import Fade from "react-reveal/Fade";
 import { Container, Row, Col, Card, Button } from "react-bootstrap";
@@ -15,12 +15,12 @@ export default function FourOhFour() {
             <img src="/images/404.svg" alt="404 error" />
           </Fade>
           <Fade bottom>
-            <h1 className="my-2">Oisann, noe rart skjedde</h1>
+            <h1 className="my-2">Woops, something strange happened</h1>
           </Fade>
           <Fade bottom>
             <p className="mx-auto">
-              Har ikke peiling på hva, er tross alt fremdeles student. Gå gjerne
-              tilbake og se på noe annet!
+              I have no idea what though, I am after all still just a student.
+              Feel free to go back and look at something else!
             </p>
           </Fade>
         </Container>
